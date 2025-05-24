b0623c008b5173bd86eec3d53377f4b48ea3e0fe
Prajesh Ananthan V <prajesh.ananthan@outlook.com>
2025-05-24 17:42:30 +0800

    Initial commit

---
+++ src/main/java/com/prajeshav/spring_mcp/SpringMcpApplication.java
@@ -0,0 +1,13 @@
+package com.prajeshav.spring_mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prajeshav/spring_mcp/SpringMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prajeshav.spring_mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
